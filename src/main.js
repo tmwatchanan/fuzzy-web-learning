76713c303cc5eccb8aa50d80cadc0b486fc453dc
tmwatchanan <tmwatchanan@gmail.com>
2017-10-27 22:42:59 +0700

    Move routes.js to router/index.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
-import { routes } from './routes'
+import { routes } from './router'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
