9472cbc2ab23a061a187207f59744764f18355a9
tmwatchanan <tmwatchanan@gmail.com>
2017-10-30 21:24:09 +0700

    Add jsLaTeX

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <nav-bar/>
-    <router-view/>
+    <router-view id="main"/>
     <footer-bar/>
   </div>
 </template>
@@ -26,5 +26,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#main {
+  margin: 0% 5% 0% 5%;
+  text-align: left;
 }
 </style>
--- src/components/Learn.vue
+++ src/components/Learn.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <p>Learn component</p>
+
+    <div class="latex">
+        \int_{0}^{\pi}\frac{x^{4}\left(1-x\right)^{4}}{1+x^{2}}dx =\frac{22}{7}-\pi
+    </div>
+
+    <hr>
+    <p class="latex">
+      \begin{aligned}x^2+4x-21&=0\\x^2+4x&=25\\x^2+4x+4&=21+4\\(x+2)^2&=25\\x+2&=\pm 5\\x&=-2\pm 5\end{aligned} 
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  
+  mounted() {
+    $(".latex").latex();
+  }
 }
 </script>
 
